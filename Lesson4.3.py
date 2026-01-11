7e44640ba32b9e045e59798f271061eafbfb8d6a
Singlesw <44473010+Singlesw@users.noreply.github.com>
2026-01-11 19:34:30 +0200

    Fix out_array indexing to include correct elements

--- Lesson4.3.py
+++ Lesson4.3.py
@@ -5,5 +5,5 @@ count = random.randint(3, 10)
 for i in range(count):
     array.append(random.randint(1, 100))
 print(f'In array  {array}')
-out_array = array[0:1] + array[2:3] + array[-1:]
+out_array = [array[0]] + [array[2]] + [array[-2]]
 print(f'Out array {out_array}')
