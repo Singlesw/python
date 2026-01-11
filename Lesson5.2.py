69ca0d3b3b201aacfffd9326462859cd90b37f8d
Singlesw <44473010+Singlesw@users.noreply.github.com>
2026-01-11 19:02:57 +0200

    Implement basic calculator functionality in Lesson5.2.py

---
+++ Lesson5.2.py
@@ -0,0 +1,19 @@
+while True:
+    num1 = float(input("Enter a number 1: "))
+    num2 = float(input("Enter a number 2: "))
+    function = input("Enter a function: ")
+    if function == "+":
+        result = num1 + num2
+    elif function == "-":
+        result = num1 - num2
+    elif function == "*":
+        result = num1 * num2
+    elif function == "/" and num2 != 0:
+        result = num1 / num2
+    else:
+        result = None
+    print(f'The result is {result}')
+    cont = input("Continue? (y/n): ")
+    if not (cont.lower() == "y" or cont.lower() == "yes"):
+        print("Bye")
+        break
