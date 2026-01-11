5d9aef798d479734040ef583e42897ce6e6d83a1
Singlesw <44473010+Singlesw@users.noreply.github.com>
2026-01-11 16:34:49 +0200

    Add random array generation and slicing

---
+++ Lesson4.3.py
@@ -0,0 +1,9 @@
+import random
+
+array = []
+count = random.randint(3, 10)
+for i in range(count):
+    array.append(random.randint(1, 100))
+print(f'In array  {array}')
+out_array = array[0:1] + array[2:3] + array[-1:]
+print(f'Out array {out_array}')
