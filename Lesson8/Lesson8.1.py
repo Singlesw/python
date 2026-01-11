4cefeae9eaf0881c688926cbb1088acb75ae47f8
Singlesw <44473010+Singlesw@users.noreply.github.com>
2026-01-11 20:07:24 +0200

    Delete Lesson8 directory

--- Lesson8/Lesson8.1.py
+++
@@ -1,7 +0,0 @@
-import string
-
-letters = input('Enter your letters: ')
-out_string = string.ascii_letters[
-    string.ascii_letters.find(letters[0]):
-    string.ascii_letters.find(letters[2]) + 1]
-print(f'Out string {out_string}')
